63604cf8b14b6de93fffcd9c578b9a82f03b7717
shiquine <godchildshiquine@gmail.com>
2021-11-03 09:50:59 -0700

    numbers

---
+++ working with numbers.py
@@ -0,0 +1,16 @@
+print(2.0987)
+print (3+4) + 5
+print (3+4)* 4
+print (3 + (4 * 3))# python follows the BODMAS method of calculations
+# we can also  stor numbers in variables
+my_num = 5
+print (my_num)
+print ((str(my_num)+ 'my favourite number') #how to change a number to a string
+print (abs(my_num)) # gives you the absolute value of a number
+print (pow(3,2)) # this gives you to the power of a number(useful for taking numbers to a specific power function)
+print (round(3.2))# this will round the number to the nearest whole number
+
+from math import* #when you add this line of code you will have access to more math problems
+
+print (floor(3.7))# this will grab the lowest number within the brackets
+print (sqrt(3.6))# gives you the squared route of a number specified
